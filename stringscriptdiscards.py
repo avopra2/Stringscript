f3c4212ca9c1b5d50c21cb70487d7c9bda6032d9
avopra2 <avopra2@AlexPC.localdomain>
2024-07-08 23:28:08 -0400

    First commit: Parsing, evaluation, serialization, return

---
+++ stringscriptdiscards.py
@@ -0,0 +1,15 @@
+# if lines[0] == "int":
+#         try:
+#                 result = runEvalInt(lines[1:])
+#         except ValueError as e:
+#                 print(e)
+# elif lines[0] == "list":
+#         try:
+#                 result = runEvalList(lines[1:])
+#         except ValueError as e:
+#                 print(e)
+# else:
+#         try:
+#                 result = runEvalStr(lines[1:])
+#         except ValueError as e:
+#                 print(e)
